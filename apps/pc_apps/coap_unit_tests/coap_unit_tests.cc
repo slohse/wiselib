eb3c913a6e1b50926dfa94712e0da0010b553964
Stephan Lohse <lohse@itm.uni-luebeck.de>
2012-02-26 17:37:32 +0100

    test cases

--- apps/pc_apps/coap_unit_tests/coap_unit_tests.cc
+++ apps/pc_apps/coap_unit_tests/coap_unit_tests.cc
@@ -15,6 +15,15 @@ using namespace wiselib;
 typedef PCOsModel Os;
 typedef wiselib::StaticString string_t;
 typedef CoapPacket<Os, UnitTestRadio, string_t>::coap_packet_t coap_packet_t;
+typedef UnitTestRadio::block_data_t block_data_t;
+
+//changing output of chars (block_data_t) to numbers
+namespace std {
+ostream &operator<<( ostream &os, const unsigned char &uc )
+{
+	return os << static_cast<unsigned>(uc);
+}
+}
 
 struct FacetsFixture {
 	FacetsFixture()
@@ -39,7 +48,7 @@ struct FacetsFixture {
 	Os::Rand::self_pointer_t rand_;
 };
 
-BOOST_FIXTURE_TEST_CASE( ConstructorsDestructors, FacetsFixture )
+BOOST_FIXTURE_TEST_CASE( Constructor, FacetsFixture )
 {
 	coap_packet_t packet;
 	BOOST_CHECK_EQUAL( packet.code(), COAP_CODE_EMPTY );
@@ -50,6 +59,40 @@ BOOST_FIXTURE_TEST_CASE( ConstructorsDestructors, FacetsFixture )
 	BOOST_CHECK_EQUAL( packet.what_options_are_set(), 0 );
 	BOOST_CHECK_EQUAL( packet.uri_port(), COAP_STD_PORT );
 
+
+}
+
+BOOST_FIXTURE_TEST_CASE( Serialize, FacetsFixture )
+{
+	coap_packet_t packet;
+	size_t vanilla_packet_serialize_length_expected = 4;
+	//just the header
+	BOOST_CHECK_EQUAL( packet.serialize_length(), vanilla_packet_serialize_length_expected );
+
+	block_data_t vanilla_packet_expected[] = { 0x50, COAP_CODE_EMPTY, 0x00, 0x00 };
+	block_data_t vanilla_packet_actual[ packet.serialize_length() ];
+	size_t vanilla_packet_serialize_length_actual = packet.serialize( vanilla_packet_actual );
+	BOOST_CHECK_EQUAL( vanilla_packet_serialize_length_actual , vanilla_packet_serialize_length_expected );
+	BOOST_CHECK_EQUAL_COLLECTIONS( vanilla_packet_actual, vanilla_packet_actual + vanilla_packet_serialize_length_expected,
+			vanilla_packet_expected, vanilla_packet_expected + vanilla_packet_serialize_length_expected );
+}
+
+BOOST_FIXTURE_TEST_CASE( Serialize2, FacetsFixture )
+{
+	coap_packet_t packet;
+	size_t packet_serialize_length_expected = 6;
+
+	packet.set_opt_if_none_match( true );
+	BOOST_CHECK_EQUAL( packet.serialize_length(), packet_serialize_length_expected );
+
+	block_data_t packet_expected[] = { 0x52, COAP_CODE_EMPTY, 0x00, 0x00,
+			( ( COAP_OPT_FENCEPOST & 0x0F) << 4 ), ( ( 7 & 0x0F) << 4 ) };
+	block_data_t packet_actual[ packet.serialize_length() ];
+	size_t packet_serialize_length_actual = packet.serialize( packet_actual );
+	BOOST_CHECK_EQUAL_COLLECTIONS( packet_actual, packet_actual + packet_serialize_length_expected,
+				packet_expected, packet_expected + packet_serialize_length_expected );
+
+	packet.set_opt_if_none_match( false );
 }
 
 
