51f2caf772f74f5756319c8fe15978b52677241e
Stephan Lohse <lohse@itm.uni-luebeck.de>
2012-03-25 18:41:56 +0200

    shit in gods beard!

--- apps/generic_apps/alignment_minimalbeispiel/example_app.cpp
+++ apps/generic_apps/alignment_minimalbeispiel/example_app.cpp
@@ -5,7 +5,6 @@
 #include "algorithms/routing/tree/tree_routing.h"
 
 #include "radio/coap/coap_packet.h"
-#include "radio/coap/coapradio.h"
 
 typedef wiselib::OSMODEL Os;
 
@@ -14,24 +13,6 @@ class ExampleApplication
 public:
 	typedef Os::Radio::block_data_t block_data_t;
 	typedef wiselib::CoapPacket<Os, Os::Radio, wiselib::StaticString> coap_packet_t;
-	typedef coap_packet_t::CoapOption<wiselib::StaticString> stringoption_t;
-
-	void check_options_equal()
-	{
-		if( coap_str_option_ == other_coap_str_option_ )
-		{
-			debug_->debug( "Options are the same!\n" );
-		}
-		else
-		{
-			debug_->debug( "Options are different!\n" );
-		}
-	}
-
-	void print_option(stringoption_t opt)
-	{
-		debug_->debug( "Option %i: %s\n", opt.option_number(), const_cast<wiselib::StaticString&>(opt.value()).c_str() );
-	}
 
 	void print_block_data( block_data_t *stream, size_t len )
 	{
@@ -54,73 +35,10 @@ public:
 
 		debug_->debug( "Example Application booting!\n" );
 
-		somestring_ = wiselib::StaticString("this string has super cow powers");
-		other_string_ = wiselib::StaticString("hi friend!");
-
-		coap_str_option_ = stringoption_t(17, somestring_ );
-		other_coap_str_option_ = stringoption_t(23, other_string_ );
-
-		check_options_equal();
-
-		somestring_ = wiselib::StaticString("Moo!");
-
-		other_coap_str_option_ = stringoption_t( 17, somestring_ );
-
-		check_options_equal();
-
-		coap_str_option_ = stringoption_t(17, somestring_ );
-
-		check_options_equal();
-
-		wiselib::list_static<Os, stringoption_t, 8> string_options_;
-
-		string_options_.push_back(coap_str_option_);
-		string_options_.push_back(other_coap_str_option_);
-
-		stringoption_t retrieved = string_options_.back();
-
-		print_option( retrieved );
-
-		string_options_.clear();
-
-		coap_str_option_ = stringoption_t(23, other_string_ );
-
-		string_options_.push_back(other_coap_str_option_);
-		string_options_.push_back(coap_str_option_);
-		retrieved = string_options_.back();
-		print_option( retrieved );
-
-		coap_packet_t packet;
-		size_t full_opaque = 8;
-		uint8_t tkn_array[ ] = { 0x56, 0x78, 0x9A, 0xBC, 0xDE, 0xF1, 0x23, 0x45 };
-		wiselib::OpaqueData tkn( tkn_array, full_opaque );
-		packet.set_token(tkn);
-
-		somestring_ = wiselib::StaticString("some/path");
-		packet.set_uri_path( somestring_ );
-
-//		block_data_t serial[30];
-
-//		size_t serial_len = packet.serialize(serial);
-
-//		print_block_data( serial, serial_len );
-
 		debug_->debug( "sizeof size_t: %i\n", sizeof(size_t) );
-		debug_->debug( "sizeof pointer: %i\n", sizeof(void*) );
-		debug_->debug( "sizeof OpaqueData: %i\n", sizeof(wiselib::OpaqueData) );
-		debug_->debug( "sizeof CoapOption<OpaqueData>: %i\n", sizeof(coap_packet_t::CoapOption<wiselib::OpaqueData>) );
-		debug_->debug( "sizeof StaticString: %i\n", sizeof(wiselib::StaticString) );
-		debug_->debug( "sizeof CoapOption<StaticString>: %i\n", sizeof(stringoption_t) );
 		debug_->debug( "sizeof uint32_t: %i\n", sizeof(uint32_t) );
-		debug_->debug( "sizeof CoapOption<uint32_t>: %i\n", sizeof(coap_packet_t::CoapOption<uint32_t>) );
-		debug_->debug( "sizeof string_list: %i\n", sizeof(wiselib::list_static<Os, stringoption_t, COAP_LIST_SIZE_STRING>) );
-		debug_->debug( "sizeof opaque_list: %i\n", sizeof(wiselib::list_static<Os, coap_packet_t::CoapOption<wiselib::OpaqueData>, COAP_LIST_SIZE_OPAQUE>) );
-		debug_->debug( "sizeof uint_list: %i\n", sizeof(wiselib::list_static<Os, coap_packet_t::CoapOption<uint32_t>, COAP_LIST_SIZE_UINT>) );
-		debug_->debug( "sizeof uint_list Groesse 1: %i\n", sizeof(wiselib::list_static<Os, coap_packet_t::CoapOption<uint32_t>, 1>) );
-		debug_->debug( "sizeof uint_list Groesse 2: %i\n", sizeof(wiselib::list_static<Os, coap_packet_t::CoapOption<uint32_t>, 2>) );
-		debug_->debug( "sizeof uint_list Groesse 3: %i\n", sizeof(wiselib::list_static<Os, coap_packet_t::CoapOption<uint32_t>, 3>) );
+		debug_->debug( "sizeof pointer: %i\n", sizeof(void*) );
 		debug_->debug( "sizeof coap_packet_t: %i\n", sizeof(coap_packet_t) );
-		debug_->debug( "sizeof CoapRadio: %i\n", sizeof(wiselib::CoapRadio<Os, Os::Radio, Os::Timer, Os::Debug, Os::Rand, wiselib::StaticString>));
 
 	}
 	// --------------------------------------------------------------------
@@ -140,8 +58,6 @@ private:
 
 	wiselib::StaticString somestring_;
 	wiselib::StaticString other_string_;
-	stringoption_t coap_str_option_;
-	stringoption_t other_coap_str_option_;
 
 };
 // --------------------------------------------------------------------------
