4c37594f4441efb84ae8da088a90891314c5004a
Stephan Lohse <lohse@itm.uni-luebeck.de>
2012-04-25 19:26:52 +0200

    muh

--- apps/generic_apps/coap_messung_bare_radio/coap_app.cpp
+++ apps/generic_apps/coap_messung_bare_radio/coap_app.cpp
@@ -3,24 +3,15 @@
  */
 #include "external_interface/external_interface.h"
 
-#include "radio/coap/coap_packet_static.h"
-#include "radio/coap/coap_service_static.h"
-
-#include "util/pstl/static_string.h"
-
 #include "stdlib.h"
 
-#define NUM_MEASUREMENTS 100
+#define NUM_MEASUREMENTS 50
 
 typedef wiselib::OSMODEL Os;
 
 class ExampleApplication
 {
 public:
-
-	typedef wiselib::CoapServiceStatic<Os, Os::Radio, Os::Timer, Os::Debug, Os::Rand, wiselib::StaticString> coap_service_t;
-	typedef coap_service_t::ReceivedMessage received_message_t;
-	typedef coap_service_t::coap_packet_t coap_packet_t;
 
 	typedef Os::Radio::node_id_t node_id_t;
 	typedef Os::Radio::block_data_t block_data_t;
@@ -34,10 +25,13 @@ public:
 
 	void tock()
 	{
-		time_t now = clock_->time();
-		int duration = ( ( clock_->seconds( now ) - clock_->seconds( tick_ ) ) * 1000 )
-				+ ( clock_->milliseconds( now ) - clock_->milliseconds( tick_ ) );
-		debug_->debug("duration %i ms", duration);
+		if(radio_->id() != server_id_ )
+		{
+			time_t now = clock_->time();
+			int duration = ( ( clock_->seconds( now ) - clock_->seconds( tick_ ) ) * 1000 )
+					+ ( clock_->milliseconds( now ) - clock_->milliseconds( tick_ ) );
+			debug_->debug("duration %i ms", duration);
+		}
 	}
 
 	void init( Os::AppMainParameter& value )
@@ -47,13 +41,19 @@ public:
 		debug_ = &wiselib::FacetProvider<Os, Os::Debug>::get_facet( value );
 		rand_ = &wiselib::FacetProvider<Os, Os::Rand>::get_facet( value );
 		clock_ = &wiselib::FacetProvider<Os, Os::Clock>::get_facet( value );
-		cservice_.init( *radio_, *timer_, *debug_, *rand_ );
-		cservice_.enable_radio();
 		measurement_counter_ = 0;
 		//
 
 		server_id_ = 0x2015;
+		ping_ = 23;
+		pong_ = 42;
 
+		radio_->reg_recv_callback<ExampleApplication,
+				&ExampleApplication::receive_radio_message > ( this );
+
+		if(radio_->id() != server_id_ )
+		timer_->set_timer<ExampleApplication,
+				&ExampleApplication::broadcast_loop>( 1000, this, NULL );
 	}
 
 	// --------------------------------------------------------------------
@@ -63,7 +63,7 @@ public:
 		{
 			debug_->debug("Tick");
 			tick();
-			cservice_.get< ExampleApplication, &ExampleApplication::receive_coap>( server_id_, temp_uri_path_, wiselib::StaticString(), this );
+			radio_->send( server_id_, sizeof(ping_), (block_data_t*) &ping_ );
 			timer_->set_timer<ExampleApplication,
 					&ExampleApplication::broadcast_loop>( 5000, this, NULL );
 		}
@@ -75,14 +75,16 @@ public:
 	// --------------------------------------------------------------------
 	void receive_radio_message( Os::Radio::node_id_t from, Os::Radio::size_t len, Os::Radio::block_data_t *buf )
 	{
-		//         debug_->debug( "received msg at %x from %x\n", radio_->id(), from );
-		//         debug_->debug( "message is %s\n", buf );
+		if( len == sizeof(uint8_t) )
+		{
+			if( *buf == ping_ )
+				radio_->send( from, sizeof(pong_), (block_data_t*) &pong_ );
+			else if ( *buf == pong_ )
+				tock();
+		}
+
 	}
 
-	void receive_coap( received_message_t & message )
-	{
-		tock();
-	}
 private:
 	Os::Radio::self_pointer_t radio_;
 	Os::Timer::self_pointer_t timer_;
@@ -91,9 +93,10 @@ private:
 	Os::Clock::self_pointer_t clock_;
 
 	node_id_t server_id_;
-	coap_service_t cservice_;
 	time_t tick_;
 	uint16_t measurement_counter_;
+	uint8_t ping_;
+	uint8_t pong_;
 
 };
 // --------------------------------------------------------------------------
